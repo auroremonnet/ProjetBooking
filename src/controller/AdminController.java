6ca377dd29b83f010d7667d24207c8f6feacd325
Hippolyte <hippolyte.nussbaumer@gmail.com>
2025-04-15 15:16:28 +0200

    Controller, Hippo travaille

--- src/controller/AdminController.java
+++ src/controller/AdminController.java
@@ -1,35 +1,31 @@
 package controller;
 
 import dao.HebergementDAO;
-import dao.ReductionDAO;
 import model.Hebergement;
-import model.Reduction;
 
 import java.sql.Connection;
 import java.util.List;
 
 public class AdminController {
     private final HebergementDAO hebergementDAO;
-    private final ReductionDAO reductionDAO;
 
     public AdminController(Connection conn) {
         this.hebergementDAO = new HebergementDAO(conn);
-        this.reductionDAO = new ReductionDAO(conn);
     }
 
     public boolean ajouterHebergement(Hebergement h) throws Exception {
         return hebergementDAO.ajouter(h);
     }
 
-    public boolean supprimerHebergement(int id) throws Exception {
-        return hebergementDAO.supprimer(id);
+    public boolean supprimerHebergement(int idHebergement) throws Exception {
+        return hebergementDAO.supprimer(idHebergement);
     }
 
-    public boolean ajouterReduction(Reduction r) throws Exception {
-        return reductionDAO.ajouterReduction(r);
+    public List<Hebergement> listerHebergements() throws Exception {
+        return hebergementDAO.getAllHebergements();
     }
 
-    public List<Reduction> listerReductions() throws Exception {
-        return reductionDAO.getAll();
+    public boolean modifierHebergement(Hebergement h) throws Exception {
+        return hebergementDAO.mettreAJour(h);
     }
 }
